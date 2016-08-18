c393efcce71905c4b56d71d8905ce4cc2ec26fb6
RenanPicoli <renanps123@gmail.com>
2016-08-18 02:34:11 -0300

    adição de defines para os endereços dos regs do nRF

--- Libraries/nrf_lib/NRF24.h
+++ Libraries/nrf_lib/NRF24.h
@@ -135,6 +135,35 @@ typedef enum{
 #define MAX_RT_MASK		0b00010000
 #define RX_P_NO_MASK	0b00001110
 
+//nRF24's registers addresses
+#define CONFIG_ADDR      0x00
+#define EN_AA_ADDR       0x01
+#define EN_RXADDR_ADDR   0x02
+#define SETUP_AW_ADDR    0x03
+#define SETUP_RETR_ADDR  0x04
+#define RF_CH_ADDR       0x05
+#define RF_SETUP_ADDR    0x06
+#define STATUS_ADDR  	 0x07
+#define OBSERVE_TX_ADDR  0x08
+#define RPD_ADDR         0x09
+#define RX_ADDR_P0_ADDR  0x0A
+#define RX_ADDR_P1_ADDR  0x0B
+#define RX_ADDR_P2_ADDR  0x0C
+#define RX_ADDR_P3_ADDR  0x0D
+#define RX_ADDR_P4_ADDR  0x0E
+#define RX_ADDR_P5_ADDR  0x0F
+#define TX_ADDR_ADDR     0x10
+#define RX_PW_P0_ADDR    0x11
+#define RX_PW_P1_ADDR    0x12
+#define RX_PW_P2_ADDR    0x13
+#define RX_PW_P3_ADDR    0x14
+#define RX_PW_P4_ADDR    0x15
+#define RX_PW_P5_ADDR    0x16
+#define FIFO_STATUS_ADDR 0x17
+#define DYNPD_ADDR       0x1C
+#define FEATURE_ADDR     0x1D
+
+
 //faz toda a configuração do TX mode e do RX mode
 typedef struct{
 	uint8_t channel; 			//determines the frequency in which the nRF42 will operate
